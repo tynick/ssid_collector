c5dabf6f06be113c24718cf152141482aa1db3ad
tynick <ballerstat@gmail.com>
2018-09-16 15:59:03 -0700

    adding new fields

--- config.py
+++ config.py
@@ -1,14 +1,17 @@
 # DB info
 database = dict(
-        url = 'db.example.com', # enter url of DB 
-        passwd = 'password' # enter DB password 
+        url = 'db.example.com',
+        name = 'ssiddb',
+        table = 'ssids',
+        user = 'username',
+        passwd = 'password'
         )
 
-# script will check here to see what url and port to check for internet connection
+# script will use this to see what url and port to check for internet connection
 conn = dict(
         url = 'google.com', # enter url
         port = '80' # enter port
         )
 
 # temp storage file
-csv = 'data_file.csv' # data will be stored here until insert to DB is possible
+csv = 'data_file.csv'
