cdb1a52b9b9cff2990ce082c2d14661da9f19975
tynick <ballerstat@gmail.com>
2018-09-16 12:22:40 -0700

    config file for ssid_collector.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# DB info
+database = dict(
+        url = 'db.example.com', # enter url of DB 
+        passwd = 'password' # enter DB password 
+        )
+
+# script will check here to see what url and port to check for internet connection
+conn = dict(
+        url = 'google.com', # enter url
+        port = '80' # enter port
+        )
+
+# temp storage file
+csv = 'data_file.csv' # data will be stored here until insert to DB is possible
